e703270588a8c2487387a99280ffa38145bd0be5
21Jasper12 <pickeng2389002@gmail.com>
2025-05-27 22:24:59 +0800

    feat: add admin index page

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,9 @@ const express = require("express");
 const router = express.Router();
 
 const restController = require("../controllers/restaurant-controller"); // 引入restaurant路由
+const admin = require("./modules/admin"); // 引入【後台】路由
+
+router.use("/admin", admin); //後台
 
 router.get("/restaurants", restController.getRestaurants);
 router.use("/", (req, res) => res.redirect("/restaurants"));

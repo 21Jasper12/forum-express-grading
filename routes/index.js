07b45e59ca3a9b856d97eaceef74514be9635138
21Jasper12 <pickeng2389002@gmail.com>
2025-06-06 00:07:17 +0800

    feat: user signup

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,17 @@
 const express = require("express");
 const router = express.Router();
 
+const userController = require("../controllers/user-controller"); //引入userControl
 const restController = require("../controllers/restaurant-controller"); // 引入restaurant路由
 const admin = require("./modules/admin"); // 引入【後台】路由
 
 router.use("/admin", admin); //後台
 
+/** userControl */
+router.get("/signup", userController.signUpPage);
+router.post("/signup", userController.signUp); //注意用 post
+
+/** restControl */
 router.get("/restaurants", restController.getRestaurants);
 router.use("/", (req, res) => res.redirect("/restaurants"));
 

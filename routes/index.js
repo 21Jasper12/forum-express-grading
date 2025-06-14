04ce7567527a31edbefe6e776f6816acc99213e0
21Jasper12 <pickeng2389002@gmail.com>
2025-06-15 00:39:45 +0800

    feat: flash msg & signup verification

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const router = express.Router();
 
+const admin = require("./modules/admin"); // 引入【後台】路由
 const userController = require("../controllers/user-controller"); //引入userControl
 const restController = require("../controllers/restaurant-controller"); // 引入restaurant路由
-const admin = require("./modules/admin"); // 引入【後台】路由
+const { generalErrorHandler } = require("../middleware/error-handler"); // 引入error-handler
 
 router.use("/admin", admin); //後台
 
@@ -14,5 +15,8 @@ router.post("/signup", userController.signUp); //注意用 post
 /** restControl */
 router.get("/restaurants", restController.getRestaurants);
 router.use("/", (req, res) => res.redirect("/restaurants"));
+
+/** error判別 */
+router.use("/", generalErrorHandler);
 
 module.exports = router;

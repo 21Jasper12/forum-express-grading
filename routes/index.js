235a9e1b2d1d777d805df1cbc7d22bd97820f16d
21Jasper12 <pickeng2389002@gmail.com>
2025-06-15 12:08:36 +0800

    feat: passport init & signin

--- routes/index.js
+++ routes/index.js
@@ -6,11 +6,27 @@ const userController = require("../controllers/user-controller"); //引入userCo
 const restController = require("../controllers/restaurant-controller"); // 引入restaurant路由
 const { generalErrorHandler } = require("../middleware/error-handler"); // 引入error-handler
 
+/** 引入passport設定 */
+const passport = require("./../config/passport");
+
 router.use("/admin", admin); //後台
 
-/** userControl */
+/** userControl - 註冊 */
 router.get("/signup", userController.signUpPage);
 router.post("/signup", userController.signUp); //注意用 post
+
+/** userControl - 登入 */
+router.get("/signin", userController.signInPage);
+router.post(
+  "/signin",
+  passport.authenticate("local", {
+    failureRedirect: "/signin",
+    failureFlash: true,
+  }),
+  userController.signIn
+);
+/** userControl - 登出 */
+router.get("/logout", userController.logout);
 
 /** restControl */
 router.get("/restaurants", restController.getRestaurants);

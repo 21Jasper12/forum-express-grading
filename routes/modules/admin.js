52f8f871f21936401a967734368e9f19fbecc681
21Jasper12 <pickeng2389002@gmail.com>
2025-06-19 23:20:00 +0800

    feat: admin restaurant page

--- routes/modules/admin.js
+++ routes/modules/admin.js
@@ -3,10 +3,12 @@ const router = express.Router();
 
 const adminController = require("../../controllers/admin-controller");
 
-/** 所有餐廳路由 - 頁面 */
-router.get("/restaurants", adminController.getRestaurants);
 /** 新增餐廳路由 - 頁面 */
 router.get("/restaurants/create", adminController.createRestaurant);
+/** 到單一餐廳【路由】 - 頁面 */
+router.get("/restaurants/:id", adminController.getRestaurant);
+/** 所有餐廳路由 - 頁面 */
+router.get("/restaurants", adminController.getRestaurants);
 /** post - 新增餐廳 */
 router.post("/restaurants", adminController.postRestaurant);
 

9368e68bc1c804c807d21cd37c67543e8608126b
21Jasper12 <pickeng2389002@gmail.com>
2025-07-12 14:25:58 +0800

    改用連線字串連線

--- config/config.js
+++ config/config.js
@@ -6,17 +6,27 @@ module.exports = {
     host: "127.0.0.1",
     dialect: "postgres",
   },
+  // production: {
+  //   username: process.env.DB_USER,
+  //   password: process.env.DB_PASS,
+  //   database: process.env.DB_NAME,
+  //   host: process.env.DB_HOST,
+  //   port: 5432,
+  //   dialect: "postgres",
+  //   dialectOptions: {
+  //     ssl: {
+  //       require: true,
+  //       rejectUnauthorized: false,
+  //     },
+  //   },
+  // },
   production: {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
-    port: 5432,
+    use_env_variable: "DATABASE_URL",
     dialect: "postgres",
     dialectOptions: {
       ssl: {
         require: true,
-        rejectUnauthorized: false,
+        rejectUnauthorized: false, // 必須為 false，否則會出現 SSL 認證錯誤
       },
     },
   },
